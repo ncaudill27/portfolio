00ca7c53c196b0e7e6d91d997d5486cb70db9420
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-09-20 03:25:18 -0400

    add path reading to headerTop

--- web/src/components/headerTop.js
+++ web/src/components/headerTop.js
@@ -4,25 +4,50 @@ import { Link } from "gatsby";
 
 import Flex from "./flex";
 
-const Header = ({siteTitle, onHideNav, onShowNav, showNav}) => (
-  <StyledHeader>
-    <StyledNav>
-      <Flex as="ul" role="list" justify="center" align="flex-end" gap={5}>
-        <li>
-          <StyledLink to="/projects/">Projects</StyledLink>
-        </li>
-        <li>
-          <Link to="/">
-            <LogoPlaceholder />
-          </Link>
-        </li>
-        <li>
-          <StyledLink to="/blog/">Blog</StyledLink>
-        </li>
-      </Flex>
-    </StyledNav>
-  </StyledHeader>
-);
+const Header = ({ siteTitle, onHideNav, onShowNav, showNav }) => {
+  const isActive = string => {
+    if (typeof window !== "undefined") {
+      console.log("path:", window.location.pathname);
+      return window.location.pathname === string;
+    }
+  };
+
+  const activeColor = (path) => (isActive(path) ? "var(--color-primary)" : "var(--color-text)");
+
+  return (
+    <StyledHeader>
+      <StyledNav>
+        <Flex as="ul" role="list" justify="center" align="flex-end" gap={5}>
+          <li>
+            <StyledLink
+              to="/projects/"
+              style={{
+                "--color": activeColor('/projects/')
+              }}
+            >
+              Projects
+            </StyledLink>
+          </li>
+          <li>
+            <Link to="/">
+              <LogoPlaceholder />
+            </Link>
+          </li>
+          <li>
+            <StyledLink
+              to="/blog/"
+              style={{
+                "--color": activeColor('/blog/')
+              }}
+            >
+              Blog
+            </StyledLink>
+          </li>
+        </Flex>
+      </StyledNav>
+    </StyledHeader>
+  );
+};
 
 const StyledHeader = styled.header`
   margin-top: 40px;
@@ -48,7 +73,7 @@ const StyledLink = styled(Link)`
   margin-bottom: var(--spacing-1);
   font-family: var(--font-family-primary);
   font-weight: var(--font-weight-medium);
-  color: inherit;
+  color: var(--color);
   font-size: 16px;
   text-decoration: none;
 `;
