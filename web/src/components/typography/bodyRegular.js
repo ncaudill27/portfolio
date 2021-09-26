4800da5d46514cdd4889b88e491c61d292fe5863
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-09-26 18:01:01 -0400

    add emDasher to default BlockRenderer

--- web/src/components/typography/bodyRegular.js
+++ web/src/components/typography/bodyRegular.js
@@ -1,7 +1,18 @@
+import React from 'react';
 import styled from "styled-components";
+import { emDasher } from '../../lib/string-utils';
+
 import { CopyWrapper } from "../project";
 
-export default styled.p`
+const Body = ({children, ...props}) => {
+  return (
+    <StyledBody {...props}>
+      {React.Children.map(children, emDasher)}
+    </StyledBody>
+  )
+}
+
+const StyledBody = styled.p`
   font-family: var(--font-family-secondary);
   font-weight: var(--font-weight-regular);
   color: var(--color-text);
@@ -16,3 +27,5 @@ export default styled.p`
     font-weight: var(--font-weight-medium);
   }
 `;
+
+export default Body
--- web/src/lib/string-utils.js
+++ web/src/lib/string-utils.js
@@ -2,7 +2,12 @@ export function ucfirst(str) {
   return `${str.substr(0, 1).toUpperCase()}${str.substr(1)}`;
 }
 
-export function emDasher(str) {
+export function emDasher(node) {
   const emRegex = /--/g;
-  return str.replaceAll(emRegex, "&#8212;");
+
+  if (typeof node === 'string') {
+    return node.replaceAll(emRegex, "&#8212;");
+  } else {
+    return node
+  }
 }
