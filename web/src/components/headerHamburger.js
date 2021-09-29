beaee7a2c4f7d545df60c9b174e4a9792ff019fd
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-09-29 01:05:27 -0400

    realize the button has to grow to fullscreen and lay links over the top ;

--- web/src/components/headerHamburger.js
+++ web/src/components/headerHamburger.js
@@ -14,14 +14,13 @@ import {
 import VisuallyHidden from "@reach/visually-hidden";
 import { useSpring, animated } from "react-spring";
 
-import "@reach/menu-button/styles.css";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleOpen = () => setIsOpen(prev => !prev);
 
   return (
-    <Menu>
+    <StyledMenu>
       <StyledButton>
         <VisuallyHidden>
           Navigation Menu
@@ -32,22 +31,40 @@ const HamburgerMenu = () => {
           toggled={isOpen}
         />
       </StyledButton>
-      <MenuList>
+      <StyledMenuList >
         <MenuLink as={Link} to="/home/">Home</MenuLink>
         <MenuLink as={Link} to="/projects/">Projects</MenuLink>
         <MenuLink as={Link} to="/blog/">Blog</MenuLink>
-      </MenuList>
-    </Menu>
+      </StyledMenuList>
+    </StyledMenu>
   );
 };
 
+const StyledMenu = styled(Menu)`
+  isolation: isolate;
+`;
+
 const StyledButton = styled(MenuButton)`
   position: fixed;
-  top: var(--spacing-0);
-  left: var(--spacing-0);
+  top: 0;
+  left: 0;
 
-  background-color: var(--color-background-alt);
+  background-color: hsl(183deg, 58%, 95%, 0.85);
+  backdrop-filter: blur(5px);
   border: none;
+  border-radius: 2px;
+
+  z-index: 1;
+`;
+
+const StyledMenuList = styled(MenuList)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: hsl(183deg, 58%, 95%, 0.85);
+  backdrop-filter: blur(5px);
 `;
 
 export default HamburgerMenu
