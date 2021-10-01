26bcbfd7db2b78e2c9d8db11a42db7062383e766
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-10-01 02:02:56 -0400

    clean up and abstract Trail

--- web/src/components/headerHamburger.js
+++ web/src/components/headerHamburger.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Link, StaticQuery, graphql, useStaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
 import { Cross as Hamburger } from "hamburger-react";
-import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 import Portal from "@reach/portal";
 import VisuallyHidden from "@reach/visually-hidden";
-import {
-  useTransition,
-  useSpring,
-  useChain,
-  animated,
-  useSpringRef,
-  config
-} from "@react-spring/web";
+import { useTransition, animated, config } from "@react-spring/web";
 import "@reach/dialog/styles.css";
 
 import Trail from "./trail";
@@ -24,54 +17,12 @@ const HamburgerMenu = () => {
   const toggleOpen = () => setIsOpen(prev => !prev);
   const close = () => setIsOpen(false);
 
-  const springApi = useSpringRef();
-  const containerStyles = useSpring({
-    ref: springApi,
-    from: { opacity: 0 },
-    to: { opacity: 1, backdropFilter: "blur(5px)" },
-    reset: () => !isOpen,
-    config: config.molasses
-  });
-
   const transitions = useTransition(isOpen, {
     from: { opacity: 0 },
     enter: { opacity: 1, backdropFilter: "blur(5px)" },
     leave: { opacity: 0 },
     config: config.slow
-    // reset: () => !isOpen
   });
-
-  const transitionApi = useSpringRef();
-  const linkTransition = useTransition(
-    [
-      <MenuLink to="/">Home</MenuLink>,
-      <MenuLink to="/projects/">Projects</MenuLink>,
-      ///spread array so that useTransition has individual elements to wrap
-      ...[
-        ...data?.projects?.edges.map(({ node }) => (
-          <SubMenuLink key={node.title} to={`/project/${node.slug.current}/`}>
-            {node.title}
-          </SubMenuLink>
-        ))
-      ],
-      <MenuLink to="/blog/">Blog</MenuLink>,
-      <MenuLink to="/contact/">Contact</MenuLink>
-    ],
-    {
-      ref: transitionApi,
-      trail: 400 / 6,
-      from: { transform: "translateX(-100%)", opacity: 0 },
-      enter: { transform: "translateX(0)", opacity: 1 },
-      leave: { transform: "translateX(-100%)", opacity: 0 },
-      reset: () => !isOpen,
-      config: config.default
-    }
-  );
-
-  useChain(isOpen ? [springApi, transitionApi] : [transitionApi, springApi], [
-    0,
-    isOpen ? 0.1 : 0.1
-  ]);
 
   return (
     <>
@@ -87,32 +38,39 @@ const HamburgerMenu = () => {
           <Hamburger label={isOpen ? "Close menu" : "Open menu"} toggled={isOpen} />
         </ExteriorButton>
       </Portal>
-          {transitions(
-            (styles, item) =>
-              item && (
-                <StyledModal
-                style={{ ...styles }}
-                aria-label="Site navigation"
-              >
-                <animated.div as={DialogContent}>
+      {transitions(
+        (styles, item) =>
+          item && (
+            <StyledModal style={{ ...styles }} aria-label="Site navigation">
+              <animated.div as={DialogContent}>
                 <ButtonBackground onClick={close}>
                   <VisuallyHidden>Close navigation menu</VisuallyHidden>
                 </ButtonBackground>
-                <Trail>
-                  <MenuLink to="/">Home</MenuLink>
-                  <MenuLink to="/projects/">Projects</MenuLink>
-                  {data?.projects?.edges.map(({ node }) => (
-                    <SubMenuLink key={node.title} to={`/project/${node.slug.current}/`}>
-                      {node.title}
-                    </SubMenuLink>
-                  ))}
-                  <MenuLink to="/blog/">Blog</MenuLink>
-                  <MenuLink to="/contact/">Contact</MenuLink>
-                </Trail>
-                </animated.div>
-              </StyledModal>
-              )
-          )}
+                <MenuList>
+                  <Trail
+                    isOpen={isOpen}
+                    config={{
+                      from: { transform: "translateX(-100%)", opacity: 0 },
+                      opacity: isOpen ? 1 : 0,
+                      transform: isOpen ? "translateX(0)" : "translateX(-100%)",
+                      config: config.stiff
+                    }}
+                  >
+                    <MenuLink to="/">Home</MenuLink>
+                    <MenuLink to="/projects/">Projects</MenuLink>
+                    {data?.projects?.edges.map(({ node }) => (
+                      <SubMenuLink key={node.title} to={`/project/${node.slug.current}/`}>
+                        {node.title}
+                      </SubMenuLink>
+                    ))}
+                    <MenuLink to="/blog/">Blog</MenuLink>
+                    <MenuLink to="/contact/">Contact</MenuLink>
+                  </Trail>
+                </MenuList>
+              </animated.div>
+            </StyledModal>
+          )
+      )}
     </>
   );
 };
