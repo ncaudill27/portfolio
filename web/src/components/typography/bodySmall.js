079e38a1d90b6d7c2e44cec7c524b5260b665643
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-11-18 16:22:50 -0500

    revise bodySmall font-size to 14px

--- web/src/components/typography/bodySmall.js
+++ web/src/components/typography/bodySmall.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export default styled.p`
-  font-size: 0.75em; // 12px
+  font-size: ${14 / 16}rem; // 12px
   font-family: var(--font-family-secondary);
   /* color: var(--color-text-alt); */
   line-height: 1;
