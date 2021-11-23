4cc7d53bfe4358dae70270887612bc4df0f12771
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-11-23 13:58:41 -0500

    move function declarations outside of useNotionElement hook

--- web/src/hooks/useNotionElement.js
+++ web/src/hooks/useNotionElement.js
@@ -3,16 +3,15 @@ import { slugify } from "../lib/string-utils";
 import Factory from "../components/factory";
 
 const useNotionElement = blocks => {
-  
   const [element, setElement] = useState({});
 
   useEffect(() => {
     let properties, tagName, children;
 
-    properties = handleProperties(blocks);
     tagName = getTag(blocks);
 
     if (blocks.type !== "text") {
+      properties = handleProperties(blocks);
       children = generateChildren(blocks);
     }
 
@@ -24,33 +23,47 @@ const useNotionElement = blocks => {
     });
   }, []);
 
-  const handleProperties = ({ properties = {}, tagName = "", children }) => {
-    let props = {};
-
-    if (properties && Object.values(properties).length > 0) {
-      for (const property in properties) {
-        props[property] = properties[property];
-      }
-    }
-
-    // conditionally add header value as is for table of contents
-    if (tagName.slice(0, 1) === "h") {
-      let id = children.length > 0 ? slugify(children[0].value) : null;
-      props = { id, as: tagName };
-    }
-
-    return props;
-  };
-
-  const getTag = ({ type, tagName }) => {
-    return !!tagName ? tagName : type;
-  };
-
-  const generateChildren = ({ children }) => {
-    return children.map((child, idx) => <Factory key={idx} blocks={child} />);
-  };
-
   return element;
+};
+
+// if text node returns type property as tagName for Factory component use
+const getTag = ({ type, tagName }) => {
+  return !!tagName ? tagName : type;
+};
+
+// will check if node has any existing properties or needs properties added
+// if neither of these conditions apply return empty object
+const handleProperties = ({ properties = {}, tagName = "", children }) => {
+  let props = {};
+
+  // grab any existing properties
+  if (Object.values(properties).length > 0) {
+    for (const property in properties) {
+      props[property] = properties[property];
+    }
+  }
+
+  if (isHeading(tagName)) props = handleHeading(tagName, children);
+
+  return props;
+};
+
+// sends children into Factory component where the next depth of nodes will enter useNotionElement
+const generateChildren = ({ children }) => {
+  return children.map((child, idx) => <Factory key={idx} blocks={child} />);
+};
+
+const isHeading = tagName => tagName.slice(0, 1) === "h";
+
+// adds id attribute for future table of contents use
+// and as attribute for styled-components
+const handleHeading = (tagName, children) => {
+  let id = children.length > 0 ? slugify(children[0].value) : null;
+
+  return {
+    id,
+    as: tagName
+  };
 };
 
 export default useNotionElement;
